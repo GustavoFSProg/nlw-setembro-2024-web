0d40594ad8866effdaff906aa11d7d7482c79293
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-16 11:36:37 -0300

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,29 @@ import './index.css'
 import { Dialog } from './components/ui/dialog'
 import Sumary from './components/sumary'
 import EmptyGoals from './components/empty-goals'
+import { useEffect, useState } from 'react'
+import CreateGoal from './components/create-goal'
 
 function App() {
+  const [summary, setSummary] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/summary')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setSummary(data.summary)
+      })
+  }, [summary])
+
   return (
     <>
       <Dialog>
-        <Sumary />
-        {/* <EmptyGoals /> */}
-        {/* <CreateGoals /> */}
+        {summary?.total > 0 ? <Sumary /> : <EmptyGoals />}
+
+        <CreateGoal />
       </Dialog>
     </>
   )
